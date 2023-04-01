3484e4ee49ea53502f127732f30e73043af5dcfb
nftxledger <contact@omarabid.com>
2023-04-01 04:57:37 +0000

    Update from https://github.com/nftxledger/frontend/commit/2fbc7ef131f01332869bf25ef1a97f457862293e

---
+++ _next/static/KuhP8rlthzbY6I8-hYn8G/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7ec020a153e111e6.js"],"/404":["static/chunks/pages/404-34cdcc5d402af01c.js"],"/_error":["static/chunks/pages/_error-f4aafbd32bd7dbac.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
