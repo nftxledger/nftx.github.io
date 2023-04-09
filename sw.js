7dbd7171093be22a65b1881452d21f5e93af5739
nftxledger <contact@omarabid.com>
2023-04-09 15:42:19 +0000

    Update from https://github.com/nftxledger/frontend/commit/2af20eb98994ff790fcc7575b9e070709512caa5

--- sw.js
+++ sw.js
@@ -1,8 +1,15 @@
-self.addEventListener("install", (event) => {
-  // This will be called when the service worker is installed.
-  event.waitUntil(() => console.info("Installed!"));
+"use strict";
+const sw = self;
+sw.addEventListener("install", (event) => {
+    // This will be called when the service worker is installed.
+    event.waitUntil(() => console.info("Installed!"));
 });
-
-self.addEventListener("fetch", function (ev) {
-  console.log("fetch", ev);
+sw.addEventListener("fetch", function (ev) {
+    console.log("updated");
+    console.log("fetch", ev);
+});
+sw.addEventListener("push", function (ev) {
+    ev.waitUntil(sw.registration.showNotification("My Title", {
+        body: "Hello world!",
+    }));
 });
